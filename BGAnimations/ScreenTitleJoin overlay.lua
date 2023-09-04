c9d095ac4925c1a318a1b079df9a447746a02185
Arthur Eubanks <ayueubanks@gmail.com>
2023-09-04 15:10:51 -0700

    Simply Love changes
    Gameplay -> Start Game
    Display exit on free play main menu
    Add operator menu secret code

--- BGAnimations/ScreenTitleJoin overlay.lua
+++ BGAnimations/ScreenTitleJoin overlay.lua
@@ -1,4 +1,12 @@
-return LoadFont("Common Bold")..{
+return Def.Actor {
+	CodeMessageCommand=function(self, param)
+		Trace("Code entered by " .. param.PlayerNumber .. " is: " .. param.Name)
+		if param.Name == "OperatorMenu" then
+			local top_screen= SCREENMAN:GetTopScreen()
+			top_screen:SetNextScreenName("ScreenOptionsService")
+			top_screen:StartTransitioningScreen("SM_GoToNextScreen")
+		end
+	end,
 	InitCommand=function(self)
 		self:xy(_screen.cx,_screen.h-80):zoom(0.7):shadowlength(0.75)
 		self:visible(false):queuecommand("Refresh")
@@ -13,7 +21,7 @@ return LoadFont("Common Bold")..{
 	CoinModeChangedMessageCommand=function(self) self:queuecommand("Refresh") end,
 
 	RefreshCommand=function(self)
-		self:visible( not IsHome() )
+		self:visible( false )
 
 		if GAMESTATE:GetCoinMode() == "CoinMode_Free" then
 		 	self:settext( THEME:GetString("ScreenTitleJoin", "Press Start") )
